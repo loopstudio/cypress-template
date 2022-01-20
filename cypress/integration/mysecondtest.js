b7ae68570946ee0413602f508fa8fb779ccb0b87
EduFerreira88 <97047770+EduFerreira88@users.noreply.github.com>
2022-01-20 11:06:25 -0300

    adding auth method

--- cypress/integration/mysecondtest.js
+++ cypress/integration/mysecondtest.js
@@ -1,8 +1,28 @@
 const { expect } = require("chai")
 
 describe('API Testing Automation Skeleton', () => {
+
+    let tokenValue;
+
+    beforeEach(() => {
+    cy.intercept({
+      method: 'GET',
+      url: 'https://suite-api.lookingglasscyber.com:8000/v1/users/00u14cs79kvsiUaZx0h8/detail',
+    }).as('authenticateEndpoint');
+
+    cy.visit('https://suite.lookingglasscyber.com/user-management');
+    cy.get('#okta-signin-username').type("eferreira@loopstudio.dev")
+    cy.get('#okta-signin-password').type("Edugol202088")
+    cy.get('#okta-signin-submit').click()
+    cy.wait('@authenticateEndpoint').then(interception => {
+      tokenValue = interception.request.headers.authorization;
+      cy.log(tokenValue);
+    })
+  })
+  
     it('GET a single post', () => {
-        const authorization = "eyJraWQiOiI5eTdZMXpTNUwtNi0xaTJfM0JLYnB3WXVQQ29tY294NjFfOWNsRnVHVWtzIiwiYWxnIjoiUlMyNTYifQ.eyJ2ZXIiOjEsImp0aSI6IkFULmpyU1Y4Zmg1a2Z3a1BWdEpNaGRQVlBuQzBPd1FiekVrSGJCcXFpUFFJUDgiLCJpc3MiOiJodHRwczovL3Nzby1kZXYubG9va2luZ2dsYXNzY3liZXIuY29tL29hdXRoMi9kZWZhdWx0IiwiYXVkIjoiYXBpOi8vZGVmYXVsdCIsImlhdCI6MTY0MjQ0MzMyOSwiZXhwIjoxNjQyNDQ2OTI5LCJjaWQiOiIwb2F6b2x0Z2UwVGUxWHZDUDBoNyIsInVpZCI6IjAwdTE0Y3M3OWt2c2lVYVp4MGg4Iiwic2NwIjpbIm9wZW5pZCIsImVtYWlsIiwicHJvZmlsZSJdLCJzdWIiOiJlZmVycmVpcmFAbG9vcHN0dWRpby5kZXYiLCJzdWl0ZV9ncm91cHMiOlsiVGVtcERldk9yZzpzY291dFBSSU1FIiwiVGVtcERldk9yZzpzY291dElOU1BFQ1QiLCJUZW1wRGV2T3JnOnNjb3V0VklTSU9OIiwiVGVtcERldk9yZzpzY291dFRIUkVBVCJdLCJhY2Nlc3NMZXZlbCI6ImFkbWluIiwib3JnIjoiVGVtcERldk9yZyIsInByZWZlcnJlZF91c2VybmFtZSI6ImVmZXJyZWlyYUBsb29wc3R1ZGlvLmRldiIsInRpbWVvdXQiOjE4MDAwMDB9.Enn-IXMcMtRmzA4kqCY58yMi07k7zEIR1x7Sehw_osokv2W9-9mmaFTI9pNYVQTM_D_xszLwt3qssZ6wVIzn2NVnxrowmWHRTlfbEke_aYWqHtznVEERBr542NLB0p6PR7be5MNYj9c-hZQjlSADqrPBBsxeFK-p7IeyN_S1YK_FWEQCuYe40L6hE_pdf1SDXv4b3Z9w4sbE1xXICOHVfjnZjaGilxBks5p4QU8M6Ypb1Cj2obgIoNqffyrxABNOiB1EblSM0pwVgzHnRdcA1TTVBL7Ot4gy-uG6qqTU6Ge3QGpGPe1i1FkCqU9KeHtYIWbIDql_EYcRp1m8Ep5V8g";
+        cy.log(tokenValue);
+        const authorization = tokenValue;
         cy.request({
           method: 'GET',
           url: 'https://suite-api.lookingglasscyber.com:8000/v1/orgs/1/licenses?status=ACTIVE',
@@ -16,5 +36,5 @@ describe('API Testing Automation Skeleton', () => {
           //expect(resp.body.title).to.eq("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
           //expect(resp.body.body).to.eq("quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto");
         })
-    })
+      })
 })
