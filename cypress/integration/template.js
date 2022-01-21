30e14e0c299db6150e24b2b8135d1c10a7c09197
EduFerreira88 <97047770+EduFerreira88@users.noreply.github.com>
2022-01-21 11:57:04 -0300

    editing template and readme

--- cypress/integration/template.js
+++ cypress/integration/template.js
@@ -1,23 +1,5 @@
 const { expect } = require("chai")
 
-describe('API Testing Automation Skeleton', () => {
-
-  beforeEach(() => {
-
-    cy.intercept({
-      method: 'POST',
-      url: 'https://suite-api.lookingglasscyber.com:8000/v1/users/00u14cs79kvsiUaZx0h8/detail',
-    }).as('authenticateEndpoint');
-
-    cy.visit('https://suite.lookingglasscyber.com/user-management');
-
-    cy.wait('@authenticateEndpoint').then(interception => {
-      tokenValue = interception.request.headers.authorization;
-      cy.log(tokenValue);
-    })
-  
-  })
-
   it('GET a single post', () => {
     cy.request({
       method: 'GET',
@@ -28,23 +10,6 @@ describe('API Testing Automation Skeleton', () => {
       expect(resp.body.id).to.eq(1);
       expect(resp.body.title).to.eq("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
       expect(resp.body.body).to.eq("quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto");
-    })
-  })
-
-  it('GET a single post', () => {
-    const authorization = tokenValue;
-    cy.request({
-      method: 'GET',
-      url: 'https://suite-api.lookingglasscyber.com:8000/v1/orgs/1/licenses?status=ACTIVE',
-      headers: {
-        authorization,
-      }
-    }).then(resp => {
-      expect(resp.status).to.eq(200);
-      //expect(resp.body.userId).to.eq(1);
-      //expect(resp.body.id).to.eq(1);
-      //expect(resp.body.title).to.eq("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
-      //expect(resp.body.body).to.eq("quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto");
     })
   })
 
@@ -99,4 +64,3 @@ describe('API Testing Automation Skeleton', () => {
       expect(resp.status).to.eq(200);
     })
   })
-})
